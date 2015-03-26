41e85cec1bb81326456abf579d731295685b1916
kswin <kimwin@Kim-Win-s-MacBook-Pro.local>
2015-03-25 22:22:49 -0400

    Clean up gulp task names

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var del = require('del');
 
-gulp.task('clean-public:swagger-ui', function(cb){
-    del(['public/api-docs/swagger-ui/dist'], cb)
+gulp.task('remove-from-public:swagger-ui', function(cb){
+    del(['public/api-docs/swagger-ui'], cb)
 });
 
-gulp.task('add-public:swagger-ui', ['clean-public:swagger-ui'], function() {
+gulp.task('add-to-public:swagger-ui', ['remove-from-public:swagger-ui'], function() {
     return gulp.src(['node_modules/swagger-ui/dist/!(index.html){,/**}'])
-    .pipe(gulp.dest('public/api-docs/swagger-ui/dist'));
+        .pipe(gulp.dest('public/api-docs/swagger-ui/dist'));
 });
 
-gulp.task('default', ['add-public:swagger-ui']);
+gulp.task('default', ['add-to-public:swagger-ui']);

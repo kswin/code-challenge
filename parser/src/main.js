882419409270c1c3cfbaccc623672c0d9ae336e9
kswin <kimwin@Kim-Win-s-MacBook-Pro.local>
2015-03-26 00:18:55 -0400

    Add function to post exercise JSON via api.

--- parser/src/main.js
+++ parser/src/main.js
@@ -3,12 +3,57 @@ var fs = require('fs'),
     split = require('split'),
     util = require('./util'),
     exports = module.exports = {},
-    file = fs.createWriteStream('output.json');
+    file = fs.createWriteStream('output.json'),
+    http = require('http');
 
 exports.csvToJson = function(filename){
     var lineNumber = 0,
         columnHeaders = [],
-        parseCsvLine;
+        parseCsvLine,
+        createExercise;
+
+    createExercise = function(exercise){
+        var exerciseString,
+            headers,
+            options;
+
+        exerciseString = JSON.stringify(exercise);
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Content-Length': exerciseString.length
+        };
+
+        options = {
+          path: '/exercises',
+          port: process.env.PORT || '3000', //TODO duplicated in bin/www
+          method: 'POST',
+          headers: headers
+        };
+
+        var request = http.request(options, function(response){
+            response.setEncoding('utf-8');
+
+            var responseString = '';
+
+            response.on('data', function(data) {
+                responseString += data;
+            });
+
+            response.on('end', function() {
+                var resultObject = JSON.parse(responseString);
+                console.log('Posted: ' + resultObject);
+            });
+        });
+
+        request.on('error', function(e) {
+            // TODO: handle error.
+        });
+
+        request.write(exerciseString);
+        request.end();
+    };
+
 
     parseCsvLine = through(function(buffer, encoding, next){
         var line = buffer.toString(),
@@ -23,9 +68,11 @@ exports.csvToJson = function(filename){
             if(lineNumber === 0 && util.areHeaderColumnsValid(columnValues)) {
                 columnHeaders = columnValues;
             } else if (lineNumber > 0 && util.areColumnsValid(columnValues)){
-                console.log(util.zipListsIntoJson(columnHeaders, columnValues));
+                var exercise = util.zipListsIntoJson(columnHeaders, columnValues);
+                createExercise(exercise);
             } else {
                 console.log('Invalid column values: ' + columnValues);
+                console.log('Skipping line: ' + lineNumber);
             }
         }
 

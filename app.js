20dacc880c6d7701403375c3cfaf949a2d55a9a0
kswin <kimwin@Kim-Win-s-MacBook-Pro.local>
2015-03-29 01:42:57 -0400

    Increase incoming data limit to support bulk imports.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var exercises = require('./rest-api/exercises/src/routes');
 var app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));

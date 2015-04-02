0f7d7db8a5fc545294cbf347896e1860da2d9ef0
kswin <kimwin@Kim-Win-s-MacBook-Pro.local>
2015-04-01 21:55:12 -0400

    Add unit tests and error handling middleware

--- rest-api/src/middleware/errors/handlers.js
+++ rest-api/src/middleware/errors/handlers.js
@@ -1,11 +1,36 @@
-exports.notFound = function(err, req, res, next){
+var BadRequest = require('./types/BadRequest');
+var NotFound = require('./types/NotFound');
 
+exports.logErrorHandler = function(err, req, res, next){
+    console.error(err.stack);
+    next(err);
 };
 
-exports.badRequest = function(err, req, res, next){
-    
+exports.notFoundHandler = function(err, req, res, next){
+    if(err instanceof NotFound) {
+        res.status(404).json({
+            code: 404,
+            message: err.message
+        });
+    } else {
+        next(err);
+    }    
 };
 
-exports.catchAll = function(err, req, res, next){
-    
+exports.badRequestHandler = function(err, req, res, next){
+    if(err instanceof BadRequest) {
+        res.status(400).json({
+            code: 400,
+            message: err.message
+        });
+    } else {
+        next(err);
+    }
+};
+
+exports.genericErrorHandler = function(err, req, res, next){
+    res.status(500).json({
+        code: 500,
+        message: err.message
+    });
 };
--- rest-api/test/middleware/errors/handlers.spec.js
+++ rest-api/test/middleware/errors/handlers.spec.js
@@ -1,1 +1,84 @@
-var r
+var expect = require('chai').expect;
+var httpMocks = require('node-mocks-http');
+var sinon = require('sinon');
+var BadRequest = require('../../../src/middleware/errors/types/BadRequest');
+var NotFound = require('../../../src/middleware/errors/types/NotFound');
+var InternalServerError = require('../../../src/middleware/errors/types/InternalServerError');
+var handlers = require('../../../src/middleware/errors/handlers');
+
+describe('[Rest Api] Middleware Error Handlers', function(){
+    var err, req, res, next;
+
+    beforeEach(function(){
+        req = undefined;
+        res = httpMocks.createResponse();
+        next = sinon.stub();
+    });
+
+    describe('notFoundHandler', function(){
+        it('should set a 404 status code', function(){
+            err = new NotFound('Entity not found.');
+
+            handlers.notFoundHandler(err, req, res, next);
+
+            expect(res.statusCode).to.be.equal(404);
+
+            expect(JSON.parse(res._getData())).to.be.deep.equal({
+                code: 404,
+                message: 'Entity not found.'
+            });
+
+            expect(res._isJSON()).to.be.true;
+        });
+
+        it('call next for other error types', function(){
+            err = new BadRequest();
+            handlers.notFoundHandler(err, req, res, next);
+            expect(next.calledWith(err)).to.be.true;
+        });
+    });
+
+    describe('badRequestHandler', function(){
+        it('should set a 400 status code', function(){
+            err = new BadRequest('Bad request.');
+
+            handlers.badRequestHandler(err, req, res, next);
+
+            expect(res.statusCode).to.be.equal(400);
+
+            expect(JSON.parse(res._getData())).to.be.deep.equal({
+                code: 400,
+                message: 'Bad request.'
+            });
+            expect(res._isJSON()).to.be.true;
+        });
+
+        it('call next other error types', function(){
+            err = new InternalServerError();
+            handlers.badRequestHandler(err, req, res, next);
+            expect(next.calledWith(err)).to.be.true;
+        });
+    });
+
+    describe('genericErrorHandler', function(){
+        it('should set a 500 status code', function(){
+            err = new InternalServerError('Server Error.');
+
+            handlers.genericErrorHandler(err, req, res, next);
+
+            expect(res.statusCode).to.be.equal(500);
+
+            expect(JSON.parse(res._getData())).to.be.deep.equal({
+                code: 500,
+                message: 'Server Error.'
+            });
+            expect(res._isJSON()).to.be.true;
+        });
+
+        it('not call nextfor other error types', function(){
+            err = new Error();
+            handlers.genericErrorHandler(err, req, res, next);
+            expect(next.calledWith(err)).to.be.false;
+        });
+    });
+});
